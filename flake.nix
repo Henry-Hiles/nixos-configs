3391168a2a56cf41d3cd6750c7c1fae389bda8b1
Henry Hiles <henry@henryhiles.com>
2023-02-06 14:28:48 -0500

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
 	inputs.nixpkgs.url = "github:NixOs/nixpkgs/nixos-unstable";
 
 	outputs = { self, nixpkgs }: {
-		nixosConfigurations.quadraticvm = nixpkgs.lib.nixosSystem {
+		nixosConfigurations.quadraticgizmo = nixpkgs.lib.nixosSystem {
 			system = "x86_64-linux";
 			modules = [ ./configuration.nix ./hardware-configuration.nix ];
 		};
